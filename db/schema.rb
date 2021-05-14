772d935f7bc34d527218ef4cfed8c7dd267571d4
Kotomyra Uchida <kotomyrau@gmail.com>
2021-05-14 10:52:43 +0100

    add dotenv

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_094833) do
+ActiveRecord::Schema.define(version: 2021_05_14_094941) do
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
