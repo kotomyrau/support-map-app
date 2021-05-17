2d9f453aba7b54d8deb73ff20fc1afc58c248484
Kotomyra Uchida <kotomyrau@gmail.com>
2021-05-17 16:56:38 +0100

    added cloudinary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,15 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @profile_user = User.find_by_username(params[:id])
+    @profile_picture = @profile_user.profile_photo
     # To appear as 'month year'
     # @member_since = @profile_user.created_at.to_date.strftime('%b %Y')
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:profile_photo)
   end
 end
