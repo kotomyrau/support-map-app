4e3c6573100ff54f775bea468b8a5143ad4898b6
Kotomyra Uchida <kotomyrau@gmail.com>
2021-05-21 11:30:13 +0100

    file clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @profile_user = User.find_by_username(params[:id])
+    @profile_user_avatar = @profile_user.user_avatar
     # To appear as 'month year'
     # @member_since = @profile_user.created_at.to_date.strftime('%b %Y')
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # before_create :set_default_avatar
   
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
@@ -9,26 +8,26 @@ class User < ApplicationRecord
   has_one_attached :avatar
   after_commit :add_default_avatar, on: %i[create update]
 
-  def avatar_thumbnail
+  def user_avatar
     if avatar.attached?
       avatar.variant(resize_to_fill: [150, 150]).processed
-    else
-      "/default-pp.jpg"
     end
   end
 
   private 
-  def add_default_avatar
 
-      unless avatar.attached?
-        avatar.attach(
-          io: File.open(
-            Rails.root.join(
-              'app', 'assets', 'images', 'default-pp.jpg'
-            )
-          ), filename: 'default-pp.jpg',
-          content_type: 'image/jpg'
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default-pp.jpg'
           )
-        end
-      end
+        ), 
+        filename: 'default-pp.jpg',
+        content_type: 'image/jpg'
+      )
+    end
+  end
+
 end
