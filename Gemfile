9d8227fc7d35ee3742203d518ee0931221539efe
Kotomyra Uchida <kotomyrau@gmail.com>
2021-05-21 11:11:50 +0100

    imagemagick added

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'dotenv-rails', groups: [:development, :test]
 gem 'simple_form'
 gem 'devise'
 gem 'cloudinary', '~> 1.16.0'
+gem 'image_processing', '~> 1.2'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,34 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :profile_photo
-
+  # before_create :set_default_avatar
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  
+  has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
 
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize_to_fill: [150, 150]).processed
+    else
+      "/default-pp.jpg"
+    end
+  end
+
+  private 
+  def add_default_avatar
+
+      unless avatar.attached?
+        avatar.attach(
+          io: File.open(
+            Rails.root.join(
+              'app', 'assets', 'images', 'default-pp.jpg'
+            )
+          ), filename: 'default-pp.jpg',
+          content_type: 'image/jpg'
+          )
+        end
+      end
 end
